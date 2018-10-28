cf2002a8c0c7798b72f6bda9fa2ec56292617724
Stefan Glorch <stefan.glorch@gym-hennef.de>
2018-10-28 23:04:33 +0100

    Main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,25 @@
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
 import javax.security.auth.login.LoginException;
 
-public class Main {
+public class Main extends ListenerAdapter {
 
     public static void main(String[] args) throws LoginException {
         JDABuilder builder = new JDABuilder(AccountType.BOT);
         String token = "MzgzOTg3ODUwODcxNDM5MzYy.Dre9jg._WKs7uv9uOlZtYbR9Rz2W7uZdtc";
         builder.setToken(token);
+        builder.addEventListener();
         builder.buildAsync();
     }
 
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        super.onMessageReceived(event);
+        if(event.getMessage().getContentRaw().equalsIgnoreCase("!ping")) {
+            event.getChannel().sendMessage("Pong!").queue();
+        }
+    }
 }
